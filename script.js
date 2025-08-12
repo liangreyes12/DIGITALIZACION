00914884448a05ce9f171d27284f6032cc3928e6
Liang Reyes <jhonwoodz675@gmail.com>
2025-08-12 17:58:37 -0500

    Calculadora

---
+++ script.js
@@ -0,0 +1,9 @@
+// const nombre = "Liang";
+// let edad = 21;
+
+// console.log(`Hola, mi nombre es ${nombre} y tengo ${edad} años.`);
+
+
+// Ejercicio de suma de dos números
+// let num1 = parseFloat(prompt("Ingrese el primer número: "));
+// let num2 = parseFloat(prompt("Ingrese el segundo numero: "));
